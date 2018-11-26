4543068a788afe920d97de08895539a3f8578939
mendesrafael965 <mendesrafael965@yahoo.com.br>
2018-11-25 23:44:21 -0200

    TP final

--- Buscador/src/Buscador.cpp
+++ Buscador/src/Buscador.cpp
@@ -11,11 +11,6 @@ Buscador::Buscador(string chave, string valor){
     elem_.insert(pair <string, string> (chave, valor));
 }
 
-Buscador::Buscador(string palavra){
-    elem_.insert(pair <string, string> ("", ""));
-    palavra_ = palavra;
-}
-
 Buscador::~Buscador(){
 
 }
@@ -35,24 +30,4 @@ vector<string> Buscador::busca(string chave){
         }
     }
     return valores_;
-}
-string Buscador::minusculo(){
-    for(unsigned int i = 0; i < palavra_.length(); i++){
-        palavra_[i] = tolower(palavra_[i]);    //toupper()–> Retorna a minúscula de uma letra
-    }
-    return palavra_;
-}
-
-string Buscador::retira_caracter(){
-    string s = palavra_;
-    unsigned int i, j = 0;
-    for(i = 0; i < palavra_.length(); i++){
-        if(isalnum(palavra_[i])){   //isalnum() -> Retorna verdadeiro se o argumento é uma letra do alfabeto ou um dígito; falso em caso contrário.
-            s[j] = palavra_[i];
-            j += 1;
-        }
-    }
-    i = (i - j);
-    s.erase(j,i);   //Apaga sub-string de j até k;
-    return s;
 }
